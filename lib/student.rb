32280c25b572486f24bf3a6a810dc8ffa7d2c198
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-25 01:49:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,8 @@ class Student
       WHERE name = ?
       SQL
       
-      DB[:conn].execute(sql, name)
+      DB[:conn].execute(sql, name).collect do |row|
+        self.new_from_db(row)
   end
   
   def save
