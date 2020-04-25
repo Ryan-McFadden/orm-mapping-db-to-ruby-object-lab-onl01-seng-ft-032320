920a5fd432b90c4f2d4a752fa1595e4a0a65ed9d
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-25 02:15:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,7 +105,14 @@ class Student
   end 
   
   def self.all_students_in_grade_X(x)
-    
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      SQL
+      
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end.first
   end 
   
 end
