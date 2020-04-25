ee0ce46914b1b746acb1becf6928536c70b00dd2
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-25 02:10:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,14 +81,14 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
       LIMIT ?
       SQL
       
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
     end
   end 
